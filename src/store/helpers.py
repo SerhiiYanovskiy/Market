0750ce3aa008dc6cdfd479c25bdc0c1741202537
@SerhiiYanovskyi <sergejanovskij71@gmail.com>
2021-07-21 12:54:32 +0300

    HW11

--- src/store/helpers.py
+++ src/store/helpers.py
@@ -6,11 +6,12 @@ def decor(func):
     return menu
 
 
+@decor
 def print_menu():
     print('0. Exit')
     print('1. List of STORE')
     print('2 Show products no more expensive than N')
-print_menu = (decor(print_menu))
+
 
 
 
