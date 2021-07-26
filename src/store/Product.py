12d11b4e82c263a00e4c282a89ded29189115f4a
@SerhiiYanovskyi <sergejanovskij71@gmail.com>
2021-07-26 10:56:03 +0300

    HW 11

---
+++ src/store/Product.py
@@ -0,0 +1,27 @@
+class Product:
+    def __init__(self, article, title, price):
+        self._article = article
+        self._title = title
+        self._prise = price
+
+    def __str__(self):
+        return f'article = {self._article} title = {self._title} prise = {self._prise}'
+
+    @property
+    def prise(self):
+        return self._prise
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/store/main.py
+++ src/store/main.py
@@ -8,13 +8,23 @@ def start_store():
         choice = int(input('Your choice'))
         if choice == 0:
             break
+
+
+        lis = []
         if choice == 1:
             for item in STORE:
-                print(item)
+                lis.append(item)
         if choice == 2:
-            N = float(input("Enter price"))
-            lis = [item for item in STORE if item.prise < N]
-            print(lis)
+            N = float(input("Yore price"))
+            lis1 = [item for item in STORE if item.prise < N]
+            for item in lis1:
+                lis.append(item)
+
+        def choice_1(S):
+            for item in S:
+               print(item)
+
+        choice_1(lis)
 
 
 if __name__ == '__main__':
